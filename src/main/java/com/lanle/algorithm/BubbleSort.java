2f1eda044c2b7e982cb6ee78ddc0b1372facc423
lanle <lanle@zzl.com>
2019-05-01 16:36:12 +0800

    Initial commit

---
+++ src/main/java/com/lanle/algorithm/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.lanle.algorithm;
+
+/**
+ * 冒泡排序
+ * 描述：外层循环排序论询次数，内层比大小
+ */
+public class BubbleSort {
+
+    /**
+     * 冒泡排序
+     * @param arrays 排序数组
+     * @param order 升序降序 0升序;1降序
+     * @return
+     */
+    public static int[] sort(int[] arrays,int order){
+        for (int i = 1; i < arrays.length; i++) {
+            for (int j = 0; j < arrays.length-i; j++) {
+                if(0 == order){
+                    if (arrays[j] > arrays[j+1]){
+                        int temp = arrays[j];
+                        arrays[j] = arrays[j+1];
+                        arrays[j+1] = temp;
+                    }
+                }else{
+                    if (arrays[j] < arrays[j+1]){
+                        int temp = arrays[j];
+                        arrays[j] = arrays[j+1];
+                        arrays[j+1] = temp;
+                    }
+                }
+            }
+        }
+        return arrays;
+    }
+
+    public static void main(String[] args) {
+        int[] testArrays = new int[]{21,12,45,32,7};
+        int[] result = BubbleSort.sort(testArrays,1);
+        for (int i : result) {
+            System.out.print(i+"\t");
+        }
+    }
+}
