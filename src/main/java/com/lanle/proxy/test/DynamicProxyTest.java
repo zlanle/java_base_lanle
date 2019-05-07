1b570d879eb474e34aa410134cc37d57b0d75df5
zlanle <zlanle@zzl.com>
2019-05-07 13:23:34 +0800

    动态代理

--- src/main/java/com/lanle/proxy/test/DynamicProxyTest.java
+++ src/main/java/com/lanle/proxy/test/DynamicProxyTest.java
@@ -6,6 +6,12 @@ import com.lanle.proxy.service.DynamicService;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 通过调用Proxy静态方法Proxy.newProxyInstance()可以创建动态代理，
+ * 这个方法需要得到一个类加载器，一个你希望该代理实现的接口列表(不是类或抽象类)，以及InvocationHandler的一个实现类。
+ * 动态代理可以将所有调用重定向到调用处理器，因此通常会调用处理器的构造器传递一个”实际”对象的引用，
+ * 从而将调用处理器在执行中介任务时，将请求转发。
+ */
 public class DynamicProxyTest {
     public static void main(String[] args) {
         DynamicServiceImpl dynamicService = new DynamicServiceImpl();
